4d23105cd8bafb989989e08deef1799e8aacc23f
Dima <dima.dibb1@gmail.com>
2020-02-25 21:41:59 -0800

    add user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const articlesRoutes = require("./articles");
+const usersRoutes = require("./users");
 
+router.use("/user", usersRoutes);
 router.use("/articles", articlesRoutes);
 
 module.exports = router;
